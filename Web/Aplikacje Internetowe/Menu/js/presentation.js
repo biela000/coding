0722d7b2d2d763f2070e044e136b1d8b2eded13e
biela000 <jak.biela000@gmail.com>
2022-03-18 22:40:06 +0100

    Fix footer, enable scrolling, style hreflinks in navigation

--- Web/Aplikacje Internetowe/Menu/js/presentation.js
+++ Web/Aplikacje Internetowe/Menu/js/presentation.js
@@ -1,6 +1,8 @@
 const slides = document.querySelectorAll('article.main-slide');
 const nav_wrapper = document.querySelector('div.nav-wrapper');
 
+document.documentElement.style.setProperty('--rowcount', `${slides.length}`)
+
 for (let i = 0; i < slides.length; i++) {
     nav_wrapper.innerHTML += `<a href="#${slides[i].id}">${i + 1}</a>`;
 }
