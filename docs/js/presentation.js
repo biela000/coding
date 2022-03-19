e01d8bdcfb60fd417dc55b31df8f996c3519560d
biela000 <jak.biela000@gmail.com>
2022-03-19 14:52:20 +0100

    Make presentation scrolling on smaller viewport better and more enjoyable

--- docs/js/presentation.js
+++ docs/js/presentation.js
@@ -1,8 +1,11 @@
+// Global variables
 const slides = document.querySelectorAll('article.main-slide');
 const nav_wrapper = document.querySelector('div.nav-wrapper');
 
+// Slides count
 document.documentElement.style.setProperty('--rowcount', `${slides.length}`)
 
+// Adding hreflinks to slides in navigation
 for (let i = 0; i < slides.length; i++) {
     nav_wrapper.innerHTML += `<a href="#${slides[i].id}">${i + 1}</a>`;
 }
