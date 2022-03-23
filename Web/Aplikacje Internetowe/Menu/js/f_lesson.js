af3852e72ff5a8c3e5f275cde0d9e2c2457bf5c0
biela000 <jak.biela000@gmail.com>
2022-03-23 22:52:32 +0100

    Add working codemirror editor

--- Web/Aplikacje Internetowe/Menu/js/f_lesson.js
+++ Web/Aplikacje Internetowe/Menu/js/f_lesson.js
@@ -1,8 +1,16 @@
 // Global variables
-const user_code_areas = document.querySelectorAll('textarea.user-code');
-const user_example_areas = document.querySelectorAll('div.user-example');
+const user_code_areas = CodeMirror(document.getElementById('user-code'), {
+    lineNumbers: true,
+    tabSize: 4,
+    matchBrackets: true,
+    mode: 'htmlmixed',
+    theme: 'nord',
+    lineWrapping: true,
+    matchBrackets: true
+});
+const user_result_areas = document.querySelectorAll('iframe.user-result');
 const finished_example_areas = document.querySelectorAll('div.finished-example');
 
 setInterval(() => {
-    user_example_areas[0].innerHTML = user_code_areas[0].value;
+    user_result_areas[0].contentWindow.document.querySelector('body').innerHTML = user_code_areas.getValue();
 }, 1000);
